4ac8b93a1a80d53907eab238e814b137c91a9c2d
Ryan Levick <ryan.levick@gmail.com>
2020-06-30 13:10:52 +0200

    Window handling

--- examples/d2d-clock/src/main.rs
+++ examples/d2d-clock/src/main.rs
@@ -3,8 +3,9 @@ use winapi::shared::minwindef::FLOAT;
 use winapi::um::winnt::HRESULT;
 
 fn main() {
-    let _ = AutoCoInitialize::new();
-    ClockWindow::new().run()
+    com::runtime::init_apartment(com::runtime::ApartmentType::SingleThreaded).unwrap();
+    ClockWindow::new().run();
+    com::runtime::deinit_apartment();
 }
 
 // this should most likely just be a wrapper type
@@ -17,25 +18,6 @@ macro_rules! HR {
         }
     };
 }
-struct AutoCoInitialize;
-impl AutoCoInitialize {
-    fn new() -> Self {
-        unsafe {
-            HR!(winapi::um::combaseapi::CoInitializeEx(
-                std::ptr::null_mut(),
-                0x2
-            ))
-        };
-        AutoCoInitialize
-    }
-}
-
-impl Drop for AutoCoInitialize {
-    fn drop(&mut self) {
-        unsafe { winapi::um::combaseapi::CoUninitialize() };
-    }
-}
-
 trait Window {
     fn run(&mut self);
 }
@@ -58,14 +40,135 @@ impl ClockWindow<DesktopWindow> {
     }
 }
 
+#[repr(C)]
 struct DesktopWindow {
     dpix: f32,
+    window: Option<winapi::shared::windef::HWND>,
 }
+
+// extern "C" {
+//     __ImageBase: winapi::um::winnt::IMAGE_DOS_HEADER ;
+// }
 
 impl DesktopWindow {
     fn new(dpix: f32) -> Self {
-        Self { dpix }
+        // WNDCLASS wc{};
+        let mut wc = winapi::um::winuser::WNDCLASSEXA::default();
+        unsafe {
+            wc.hCursor = winapi::um::winuser::LoadCursorW(
+                std::ptr::null_mut(),
+                winapi::um::winuser::IDC_ARROW,
+            );
+            // wc.hInstance = reinterpret_cast<HINSTANCE>(&__ImageBase);
+            wc.lpszClassName = b"Sample" as *const u8 as _;
+            wc.style = winapi::um::winuser::CS_HREDRAW | winapi::um::winuser::CS_VREDRAW;
+            wc.lpfnWndProc = Some(window_proc);
+        }
+        // RegisterClass(&wc);
+
+        // CreateWindow(wc.lpszClassName,
+        //     L"Clock",
+        //     WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        //     CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        //     nullptr, nullptr, wc.hInstance, this);
+        Self { dpix, window: None }
     }
+    unsafe fn message_handler(
+        &self,
+        message: u32,
+        wparam: winapi::shared::minwindef::WPARAM,
+        lparam: winapi::shared::minwindef::LPARAM,
+    ) -> winapi::shared::minwindef::LRESULT {
+        match message {
+            winapi::um::winuser::WM_DESTROY => {
+                winapi::um::winuser::PostQuitMessage(0);
+                0
+            }
+            winapi::um::winuser::WM_PAINT => {
+                // PAINTSTRUCT ps;
+                // check_bool(BeginPaint(m_window, &ps));
+                // render();
+                // EndPaint(m_window, &ps);
+                0
+            }
+            winapi::um::winuser::WM_SIZE => {
+                // if m_target && SIZE_MINIMIZED != wparam
+                // {
+                //     resize_swapchain_bitmap();
+                //     render();
+                // }
+
+                0
+            }
+            winapi::um::winuser::WM_DISPLAYCHANGE => {
+                // render();
+                0
+            }
+            winapi::um::winuser::WM_USER => {
+                // if (S_OK == m_swapChain->Present(0, DXGI_PRESENT_TEST))
+                // {
+                //     m_dxfactory->UnregisterOcclusionStatus(m_occlusion);
+                //     m_occlusion = 0;
+                //     m_visible = true;
+                // }
+
+                0
+            }
+            winapi::um::winuser::WM_POWERBROADCAST => {
+                // auto const ps = reinterpret_cast<POWERBROADCAST_SETTING*>(lparam);
+                // m_visible = 0 != *reinterpret_cast<DWORD const*>(ps->Data);
+
+                // if (m_visible)
+                // {
+                //     PostMessage(m_window, WM_NULL, 0, 0);
+                // }
+
+                winapi::shared::minwindef::TRUE as isize
+            }
+            winapi::um::winuser::WM_ACTIVATE => {
+                // m_visible = !HIWORD(wparam);
+                0
+            }
+            winapi::um::winuser::WM_GETMINMAXINFO => {
+                // auto info = reinterpret_cast<MINMAXINFO*>(lparam);
+                // info->ptMinTrackSize.y = 200;
+
+                0
+            }
+            _ => winapi::um::winuser::DefWindowProcW(self.window(), message, wparam, lparam),
+        }
+    }
+
+    fn window(&self) -> winapi::shared::windef::HWND {
+        self.window.expect("Tried to use window before it was set")
+    }
+}
+
+unsafe extern "system" fn window_proc(
+    window: winapi::shared::windef::HWND,
+    message: u32,
+    wparam: winapi::shared::minwindef::WPARAM,
+    lparam: winapi::shared::minwindef::LPARAM,
+) -> winapi::shared::minwindef::LRESULT {
+    if winapi::um::winuser::WM_NCCREATE == message {
+        let cs = lparam as *mut winapi::um::winuser::CREATESTRUCTW;
+        let that = (*cs).lpCreateParams as *mut DesktopWindow;
+        (*that).window = Some(window);
+        winapi::um::winuser::SetWindowLongPtrW(
+            window,
+            winapi::um::winuser::GWLP_USERDATA,
+            that as isize,
+        );
+    } else {
+        let that =
+            winapi::um::winuser::GetWindowLongPtrW(window, winapi::um::winuser::GWLP_USERDATA);
+        let that = that as usize as *const DesktopWindow;
+        if !that.is_null() {
+            return (*that).message_handler(message, wparam, lparam);
+        }
+    }
+
+    winapi::um::winuser::DefWindowProcW(window, message, wparam, lparam)
 }
 
 impl Window for DesktopWindow {
@@ -84,9 +187,7 @@ impl Window for DesktopWindow {
             factory.get_desktop_dpi(&mut self.dpix, &mut dpiy);
         }
         println!("DPI: {}x{}", self.dpix, dpiy);
-        // TODO create device independent resources: CreateDeviceIndependentResources
-
-        // TODO: VERIFY(__super::Create(nullptr, bounds, L"Direct2D"));
+        // TODO create device independent resources: create_device_independent_resources
 
         // winapi::um::winuser::RegisterPowerSettingNotification
     }
